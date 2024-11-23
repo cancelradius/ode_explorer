8003dc5dcbfe12d461784353706e5dda398da89e
cancelradius <felixrottermccartney@gmail.com>
2024-11-23 18:45:00 -0500

    New test functions

--- sym_diff.py
+++ sym_diff.py
@@ -7,11 +7,11 @@ sp.init_printing( use_latex='mathjax' )
 
 t = sp.var('t')
 f = sp.Function('f')
-diffeq = sp.Eq(sp.harmonic(f(t)), sp.diff(f(t), t))
+diffeq = sp.Eq(sp.tanh(sp.euler(f(t))), sp.diff(f(t), t)**-1)
 
 def euler(q, dt, ti, tf, qi, *argv):
     dfdt = sp.solve(q, sp.diff(f(t), t))
-    dfdt_eval = lambda x: sp.N(dfdt[0].subs(f(t), x))
+    dfdt_eval = lambda x: sp.N(sp.re(dfdt[0].subs(f(t), x)))
 
     steps = int((tf-ti)/dt)
     f_approx = np.empty(steps+1)
@@ -23,7 +23,7 @@ def euler(q, dt, ti, tf, qi, *argv):
     return(f_approx)
 
 #%%
-eul = euler(diffeq, 0.00001, 0, 10, 1)
-mpl.plot(np.arange(0, 10.00001, 0.00001), eul)
+eul = euler(diffeq, 0.00001, 0, 2, 0)
+mpl.plot(np.arange(0, 2.00001, 0.00001), eul)
 
 print(eul)
